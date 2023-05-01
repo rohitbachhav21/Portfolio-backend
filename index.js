1e0f3e34c184a44003d11ff0e0beb4e5d4ef153a
Rohit Bachhav <bachhavrohit21@gmail.com>
2023-05-01 21:03:13 +0100

    Forth Commit

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-require('dotenv').config();
 
 ;
 
